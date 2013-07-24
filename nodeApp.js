aa8e2c42d97273b6d9894a22538c206e523be0b6
hung <hung.chen.ucf@gmail.com>
2013-07-24 14:10:09 -0600

    updated some quick things and should start up heroku

--- nodeApp.js
+++ nodeApp.js
@@ -1,8 +1,19 @@
 var express = require("express");
+var http = require("http");
+var https = require("https");
 var app = express();
 
 
 app.use(express.static(__dirname));
 
 
-app.listen(3000);
+http.createServer(app).listen(3000);
+
+/*
+	need to set this up at some point....
+// This line is from the Node.js HTTPS documentation.
+var options = {
+  key: fs.readFileSync('test/fixtures/keys/agent2-key.pem'),
+  cert: fs.readFileSync('test/fixtures/keys/agent2-cert.pem')
+};
+https.createServer(options, app).listen(443);*/
