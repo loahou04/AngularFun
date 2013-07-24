fffa8a3387528f859fbb713fddee4a908d2ece45
hung <hung.chen.ucf@gmail.com>
2013-07-24 14:35:30 -0600

    added some quick logging for myself

--- nodeApp.js
+++ nodeApp.js
@@ -7,13 +7,7 @@ var app = express();
 app.use(express.static(__dirname));
 
 
-http.createServer(app).listen(3000);
-
-/*
-	need to set this up at some point....
-// This line is from the Node.js HTTPS documentation.
-var options = {
-  key: fs.readFileSync('test/fixtures/keys/agent2-key.pem'),
-  cert: fs.readFileSync('test/fixtures/keys/agent2-cert.pem')
-};
-https.createServer(options, app).listen(443);*/
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+	console.log("Listening on: " + port);
+});
